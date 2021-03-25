d18f8c0f7bae1ae073b4971be78c87261075aea7
Vicente Reyes Chavarro <reyesvrc@gmail.com>
2021-03-25 11:33:30 -0500

    modificaciones de estilos

--- WebAppEcommerce/ClientApp/src/components/pagina/index.js
+++ WebAppEcommerce/ClientApp/src/components/pagina/index.js
@@ -14,8 +14,7 @@ import imageHeader2 from '../../imagenes/pagina/header2.jpg';
 import imageHeader3 from '../../imagenes/pagina/header3.jpg';
 import { Link } from "react-router-dom";
 import { Row, Col } from 'react-bootstrap';
-
-
+import AddIcon from '@material-ui/icons/Add';
 
 
 
@@ -53,20 +52,22 @@ class Pagina extends Component {
                 </section>
                 <section id="container" className="container mt-lg-3 mb-lg-5">
                     <div className="row smart-order">
-                        <div className="col   text-center text-uppercase pb-4">
+                        <div className="col   text-center text-uppercase pt-2 pb-4">
                             <h6>EMPIEZA TU PEDIDO</h6>
                             <div className="icons-pedido">
                                 <Link to="/pideLinea" className="btn btn-default btn-3d-style  btn-block" >
-                                    Entrega a domicilio
+                                    <span className="element-hide"><AddIcon  /></span>
+                                    <span className="hidde-element"> Entrega a domicilio</span>
+                                   
                                 </Link>
                              
                             </div>
                         </div>
                       
                     </div>
-                    <div className="row">
+                    <div className="row p-4 nav-icon rounded">
                         <div className="col-12 col-md-6 col-lg-4 mb-4">
-                            <div className="card">
+                            <div className="card shadow-sm">
                                 <img className="card-img-top" src={imageHeader1} alt="Foto de Pizza 1" />
                                 <div className="card-body">
                                     <h5 className="card-title">Pizza 1</h5>
@@ -75,7 +76,7 @@ class Pagina extends Component {
                              </div>
                             </div>
                         <div className="col-12 col-md-6 col-lg-4 mb-4">
-                            <div className="card">
+                            <div className="card shadow-sm">
                                 <img className="card-img-top" src={imageHeader2} alt="Foto de Pizza 2"/>
                                 <div className="card-body">
                                     <h5 className="card-title">Pizza 2</h5>
@@ -84,14 +85,15 @@ class Pagina extends Component {
                                    </div>
                                 </div>
                         <div className="col-12 col-md-6 col-lg-4 mb-4">
-                            <div className="card">
+                            <div className="card shadow-sm">
                                 <img className="card-img-top" src={imageHeader3} alt="Foto de Pizza 3"/>
                                 <div className="card-body">
                                     <h5 className="card-title">Pizza 3</h5>
                                     <p className="card-text">Vivamus quam mi, egestas eu metus id, mollis suscipit ipsum. In vel mollis mauris. Nunc id sem a lacus tincidunt pretium in in urna. Nam mi nisi, tincidunt ac tincidunt id, fermentum at metus.</p>
                                             </div>
                                       </div>
-                                    </div>
+                        </div>
+                       
                                 </div>
                 </section>
                 
