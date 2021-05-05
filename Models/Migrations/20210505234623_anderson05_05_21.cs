edcb4b30f009b8f2ef2149b93ee637303914c04c
andersonrugeles <anderson.rugeles0606@gmail.com>
2021-05-05 18:49:14 -0500

    Maxlenght Banner

---
+++ Models/Migrations/20210505234623_anderson05_05_21.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Models.Migrations
+{
+    public partial class anderson05_05_21 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Banners",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Banners",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+        }
+    }
+}
